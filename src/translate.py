f589d484c23fd1931d8e9e497e8d9d3bbfc6f5c5
EC2 Default User <ec2-user@ip-172-31-17-116.us-west-2.compute.internal>
2020-04-14 15:26:01 +0000

    add python file

---
+++ src/translate.py
@@ -0,0 +1,10 @@
+import boto3
+
+client = boto3.client('translate', region_name = "us-west-2")
+
+text = "Hola mi nombre es Savan"
+
+result = client.translate_text(Text=text, SourceLanguageCode="auto",
+    TargetLanguageCode="en")
+    
+print(result["TranslatedText"])
